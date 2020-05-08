e70c13e0d5b3cade4f5e8ba9a24015dfc4c4cdc0
SinghSujitkumar <2017.sujitkumar.singh@ves.ac.in>
2020-05-08 19:46:44 +0530

    Initalcommit

---
+++ src/Hello.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    println("Hello, ${args[0]}")
+    val isUnit = println("This is an expression")
+    println(isUnit)
+//    Temperature
+//    val temperature = 10
+//    val isHot = if (temperature > 50) true else false
+//    println(isHot)
+
+    val temperature = 60
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
+}
+//Everything is a unit
